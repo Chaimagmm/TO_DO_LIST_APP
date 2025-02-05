0000cbd9863f71d641f31366aebd0d5c97209f61
Chaima <chaimagmm99@gmail.com>
2025-02-05 15:58:30 +0100

    Adding the delete button

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
+
+document.getElementById("display").addEventListener("keydown", function(){
+    if(event.key === "Enter")
+        addTask()
+})
+
+
 function addTask(){
     let newTask = document.createElement("li")
     let data = document.getElementById("display")
     let content = document.createTextNode(data.value)
+    //creating a button to delete the task
+    let deletebtn = document.createElement("button")
+    deletebtn.classList.add("delete-btn")
+    //deletebtn.appendChild(document.createTextNode("Delete"))
+    let icon = document.createElement("img")
+    icon.src="./trash.png"
+    icon.alt="Delete"
+    deletebtn.appendChild(icon)
+    deletebtn.addEventListener("click", function(){
+        this.parentElement.remove();
+    })
+   
+    //creating an underline
+    let line = document.createElement("hr")
 
     newTask.appendChild(content)
+    newTask.appendChild(deletebtn)
+    newTask.appendChild(line)
 
     let element = document.getElementById("ul");
     element.appendChild(newTask);
